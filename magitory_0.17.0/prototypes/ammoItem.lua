3c353cd42c7dafad652e31c8ff138b20facddc3d
Johannes Walter <Hannibali@gmx.de>
2020-03-12 14:57:47 +0100

    new spells without function and placeholder recipiecs

--- magitory_0.17.0/prototypes/ammoItem.lua
+++ magitory_0.17.0/prototypes/ammoItem.lua
@@ -14,10 +14,10 @@ testentity =
 	
 }
 
-air_spell = 
+wind_spell = 
 {
 	type = "ammo",
-	name = "air_spell",
+	name = "wind_spell",
 	icon = "__magitory__/graphics/icons/liquid-mana.png",
 	icon_size = 32,
 	order = "ad",
@@ -50,10 +50,231 @@ air_spell =
 	}
 }
 
+water_spell = 
+{
+	type = "ammo",
+	name = "water_spell",
+	icon = "__magitory__/graphics/icons/liquid-mana.png",
+	icon_size = 32,
+	order = "ad",
+	stack_size = 50,
+	ammo_type = 
+	{
+		category = "spell",
+		action = 
+		{
+			type = "area",
+			radius = 15,
+			collision_mode = "distance-from-center",
+			action_delivery =
+			{
+				type = "instant",
+				target_effects = 
+				{
+					type = "damage",
+					damage = 
+					{
+						amount = 0,
+						type = "magic_water",
+					}	
+				}
+			}
+		}
+		--range = 1000,
+		--cooldown = 1,
+		
+	}
+}
+
+void_spell = 
+{
+	type = "ammo",
+	name = "void_spell",
+	icon = "__magitory__/graphics/icons/liquid-mana.png",
+	icon_size = 32,
+	order = "ad",
+	stack_size = 50,
+	ammo_type = 
+	{
+		category = "spell",
+		action = 
+		{
+			type = "area",
+			radius = 15,
+			collision_mode = "distance-from-center",
+			action_delivery =
+			{
+				type = "instant",
+				target_effects = 
+				{
+					type = "damage",
+					damage = 
+					{
+						amount = 0,
+						type = "magic_void",
+					}	
+				}
+			}
+		}
+		--range = 1000,
+		--cooldown = 1,
+		
+	}
+}
+
+stone_spell = 
+{
+	type = "ammo",
+	name = "stone_spell",
+	icon = "__magitory__/graphics/icons/liquid-mana.png",
+	icon_size = 32,
+	order = "ad",
+	stack_size = 50,
+	ammo_type = 
+	{
+		category = "spell",
+		action = 
+		{
+			type = "area",
+			radius = 15,
+			collision_mode = "distance-from-center",
+			action_delivery =
+			{
+				type = "instant",
+				target_effects = 
+				{
+					type = "damage",
+					damage = 
+					{
+						amount = 0,
+						type = "magic_stone",
+					}	
+				}
+			}
+		}
+		--range = 1000,
+		--cooldown = 1,
+		
+	}
+}
+
+fire_spell = 
+{
+	type = "ammo",
+	name = "fire_spell",
+	icon = "__magitory__/graphics/icons/liquid-mana.png",
+	icon_size = 32,
+	order = "ad",
+	stack_size = 50,
+	ammo_type = 
+	{
+		category = "spell",
+		action = 
+		{
+			type = "area",
+			radius = 15,
+			collision_mode = "distance-from-center",
+			action_delivery =
+			{
+				type = "instant",
+				target_effects = 
+				{
+					type = "damage",
+					damage = 
+					{
+						amount = 0,
+						type = "magic_fire",
+					}	
+				}
+			}
+		}
+		--range = 1000,
+		--cooldown = 1,
+		
+	}
+}
+
+nature_spell = 
+{
+	type = "ammo",
+	name = "nature_spell",
+	icon = "__magitory__/graphics/icons/liquid-mana.png",
+	icon_size = 32,
+	order = "ad",
+	stack_size = 50,
+	ammo_type = 
+	{
+		category = "spell",
+		action = 
+		{
+			type = "area",
+			radius = 15,
+			collision_mode = "distance-from-center",
+			action_delivery =
+			{
+				type = "instant",
+				target_effects = 
+				{
+					type = "damage",
+					damage = 
+					{
+						amount = 0,
+						type = "magic_nature",
+					}	
+				}
+			}
+		}
+		--range = 1000,
+		--cooldown = 1,
+		
+	}
+}
+
+ice_spell = 
+{
+	type = "ammo",
+	name = "ice_spell",
+	icon = "__magitory__/graphics/icons/liquid-mana.png",
+	icon_size = 32,
+	order = "ad",
+	stack_size = 50,
+	ammo_type = 
+	{
+		category = "spell",
+		action = 
+		{
+			type = "area",
+			radius = 15,
+			collision_mode = "distance-from-center",
+			action_delivery =
+			{
+				type = "instant",
+				target_effects = 
+				{
+					type = "damage",
+					damage = 
+					{
+						amount = 0,
+						type = "magic_ice",
+					}	
+				}
+			}
+		}
+		--range = 1000,
+		--cooldown = 1,
+		
+	}
+}
+
 
 
 
 data:extend({
-	air_spell,
-	damage_type,
+	wind_spell,
+	water_spell,
+	void_spell,
+	stone_spell,
+	fire_spell,
+	nature_spell,
+	ice_spell,
 })
--- magitory_0.17.0/prototypes/recipe.lua
+++ magitory_0.17.0/prototypes/recipe.lua
@@ -21,10 +21,10 @@ magitory_subgroup =
 	group = "magitory-base",
 }
 
-air_spell_recipe = 
+wind_spell_recipe = 
 {
 	type = "recipe",
-	name = "air_spell_recipe",
+	name = "wind_spell_recipe",
 	category = "crafting",
 	enabled = true,
 	
@@ -40,7 +40,157 @@ air_spell_recipe =
 	{
 		{
 			type="item",
-			name="air_spell",
+			name="wind_spell",
+			amount=1
+		}
+	}	
+}
+
+water_spell_recipe = 
+{
+	type = "recipe",
+	name = "water_spell_recipe",
+	category = "crafting",
+	enabled = true,
+	
+	ingredients =
+	{
+		{
+			type="item",
+			name="wood",
+			amount=1
+		}
+	},
+	results=
+	{
+		{
+			type="item",
+			name="water_spell",
+			amount=1
+		}
+	}	
+}
+
+void_spell_recipe = 
+{
+	type = "recipe",
+	name = "void_spell_recipe",
+	category = "crafting",
+	enabled = true,
+	
+	ingredients =
+	{
+		{
+			type="item",
+			name="wood",
+			amount=1
+		}
+	},
+	results=
+	{
+		{
+			type="item",
+			name="void_spell",
+			amount=1
+		}
+	}	
+}
+
+stone_spell_recipe = 
+{
+	type = "recipe",
+	name = "stone_spell_recipe",
+	category = "crafting",
+	enabled = true,
+	
+	ingredients =
+	{
+		{
+			type="item",
+			name="wood",
+			amount=1
+		}
+	},
+	results=
+	{
+		{
+			type="item",
+			name="stone_spell",
+			amount=1
+		}
+	}	
+}
+
+fire_spell_recipe = 
+{
+	type = "recipe",
+	name = "fire_spell_recipe",
+	category = "crafting",
+	enabled = true,
+	
+	ingredients =
+	{
+		{
+			type="item",
+			name="wood",
+			amount=1
+		}
+	},
+	results=
+	{
+		{
+			type="item",
+			name="fire_spell",
+			amount=1
+		}
+	}	
+}
+
+nature_spell_recipe = 
+{
+	type = "recipe",
+	name = "nature_spell_recipe",
+	category = "crafting",
+	enabled = true,
+	
+	ingredients =
+	{
+		{
+			type="item",
+			name="wood",
+			amount=1
+		}
+	},
+	results=
+	{
+		{
+			type="item",
+			name="nature_spell",
+			amount=1
+		}
+	}	
+}
+
+ice_spell_recipe = 
+{
+	type = "recipe",
+	name = "ice_spell_recipe",
+	category = "crafting",
+	enabled = true,
+	
+	ingredients =
+	{
+		{
+			type="item",
+			name="wood",
+			amount=1
+		}
+	},
+	results=
+	{
+		{
+			type="item",
+			name="ice_spell",
 			amount=1
 		}
 	}	
@@ -112,7 +262,6 @@ liquid_mana_recipe =
 	  tertiary = {r = 0.0, g = 0.216, b = 0.866, a = 0.000}, -- #dd5d0000
 	}
 }
-
 
 alien_science_pack_mk1_recipe = 
 {
@@ -198,8 +347,6 @@ alien_science_pack_mk2_recipe =
 	}
 }
 
-
-
 data:extend({
 	magitory_category,
 	magitory_group,
@@ -208,5 +355,12 @@ data:extend({
 	alien_science_pack_mk1_recipe,
 	alien_science_pack_mk2_recipe,
 	hazelnut_ward_recipe,
-	air_spell_recipe
+	wind_spell_recipe,
+	water_spell_recipe,
+	void_spell_recipe,
+	stone_spell_recipe,
+	fire_spell_recipe,
+	nature_spell_recipe,
+	ice_spell_recipe,
+	
 })
