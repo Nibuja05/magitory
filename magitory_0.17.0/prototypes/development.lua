3a042f2ed0bbf2b06e2639d809d7994fdf8c89fe
Johannes Walter <Hannibali@gmx.de>
2020-03-13 20:55:35 +0100

    update

--- magitory_0.17.0/prototypes/development.lua
+++ magitory_0.17.0/prototypes/development.lua
@@ -1,10 +1,8 @@
 testiteam = {
     type = "item-with-inventory",
-    name = "test-item-with-inventory",
+    name = "test_gloves",
     icon = "__magitory__/graphics/icons/SciencePackMk1.png",
 	icon_size = 32,
-	flags = {"mod-openable"},
-    --subgroup = "science-pack",
     stack_size = 1,
 	inventory_size = 3,
 }
@@ -30,9 +28,25 @@ testrecipe =
 			type="item",
 			name="test-item-with-inventory",
 			amount=1
+		},
+		{
+			type="item",
+			name="test_ring_speed",
+			amount=1
+		},
+		{
+			type="item",
+			name="test_ring_armor",
+			amount=1
+		},
+		{
+			type="item",
+			name="test_ring_strengh",
+			amount=1
 		}
 	}	
 }
+
 
 testring = {
     type = "item",
@@ -42,12 +56,32 @@ testring = {
 	--flags = {"goes-to-quickbar"},
     --subgroup = "science-pack",
     stack_size = 1,
-	inventory_size = 3,
 }
 
+testring2 = {
+    type = "item",
+    name = "test_ring_armor",
+    icon = "__magitory__/graphics/icons/SciencePackMk1.png",
+	icon_size = 32,
+	--flags = {"goes-to-quickbar"},
+    --subgroup = "science-pack",
+    stack_size = 1,
+}
+
+testring3 = {
+    type = "item",
+    name = "test_ring_strengh",
+    icon = "__magitory__/graphics/icons/SciencePackMk1.png",
+	icon_size = 32,
+	--flags = {"goes-to-quickbar"},
+    --subgroup = "science-pack",
+    stack_size = 1,
+}
 
 data:extend({
 	testiteam,
 	testrecipe,
 	testring,
+	testring2,
+	testring3,
 })
--- magitory_0.17.0/script/developmentScript.lua
+++ magitory_0.17.0/script/developmentScript.lua
@@ -1,6 +1,17 @@
+
+
+
+
+
 script.on_event(defines.events.on_gui_closed,
 	function(event)
-	if
-		game.print(event.player_index)
+	
+		if event.gui_type== defines.gui_type.item then
+			contents = event.item.get_inventory(defines.inventory.item_main).get_contents()
+			
+			for pair1,pair2 in pairs(contents) do
+				game.print(pair1)
+			end
+		end
 	end
 )
